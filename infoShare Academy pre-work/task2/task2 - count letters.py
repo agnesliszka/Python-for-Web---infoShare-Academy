91e4bff827988477730f9017bfb632db6b735027
Agnieszka <agnesliszk@gmail.com>
2019-02-28 12:12:03 +0100

    while updated

--- infoShare Academy pre-work/task2/task2 - count letters.py
+++ infoShare Academy pre-work/task2/task2 - count letters.py
@@ -9,7 +9,7 @@ alphabet = dict.fromkeys(string.ascii_lowercase, 0)
 # Count how many times each alphabet letter appears in word_{i}.txt files
 i = 0
 
-while i != 5:
+while i != 30:
     with open(f'word_{i}.txt','r') as file:
         for word in file:
             for char in word:
