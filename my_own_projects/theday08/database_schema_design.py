0f91f1a0bd4402bbe9b7fbd27096df4c471b1c0c
Agnieszka <agnesliszk@gmail.com>
2019-03-26 21:59:56 +0100

    Homework day08 files created

---
+++ my_own_projects/theday08/database_schema_design.py
@@ -0,0 +1,71 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+# Create 'kampanie' database schema
+class Campaign(Base):
+    # Set a table name
+    __tablename__ = 'campaign'
+
+    # Primary key
+    id = Column(Integer, primary_key=True)
+
+    # Foreign key
+    portal_id = Column(Integer, ForeignKey('portal.id'))
+
+    # Additional columns
+    date = Column(String)
+    api_type = Column(String)
+
+    # Creating a relation between tables
+    portal = relationship('portal', back_populates='campaign')
+    offers = relationship('offers', back_populates='campaign')
+
+class Portal(Base):
+    # Set a table name
+    __tablename__ = 'portal'
+
+    # Primary key
+    id = Column(Integer, primary_key=True)
+
+    # Additional columns
+    name = Column(String)
+
+    # Creating a relation between tables
+    campaign = relationship('campaign', back_populates='portal')
+
+class Offers(Base):
+    # Set a table name
+    __tablename__ = 'offers'
+
+    # Primary key
+    id = Column(Integer, primary_key=True)
+
+    # Foreign key
+    campaign_id = Column(Integer, ForeignKey('campaign.id'))
+
+    # Additional columns
+    offer_id = Column(String)
+    seller_id = Column(String)
+    location = Column(String)
+    title = Column(String)
+    price = Column(String)
+    brand = Column(String)
+    design = Column(String)
+    title = Column(String)
+    production_year = Column(String)
+    course = Column(String)
+    production_year = Column(String)
+    capacity = Column(String)
+    power = Column(String)
+    fuel_type = Column(String)
+    colour = Column(String)
+    damaged = Column(String)
+    country = Column(String)
+    driving_gear = Column(String)
+    number_of_seats = Column(String)
+
+    # Creating a relation between tables
+    campaign = relationship('campaign', back_populates='offers')
