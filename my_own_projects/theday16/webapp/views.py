20d21058cd610125fdd87a3543fb27279251c650
Agnieszka <agnesliszk@gmail.com>
2019-04-26 14:50:30 +0200

    Homework day16 files added

--- my_own_projects/theday16/webapp/views.py
+++ my_own_projects/theday16/webapp/views.py
@@ -5,28 +5,42 @@ from . import app
 from. import db
 
 
-@app.route('/oferty', methods=['GET', 'POST'])
-def add_client():
-    form = OfferForm()
+@app.route('/offers')
+def get_offers():
+    # Get data from offers table
+    offers = Offer.query.all()
 
-    if form.validate_on_submit():
-        validated_data = form.data.copy()
-        validated_data.pop('csrf_token')
+    # Create data provided to the template
+    data = {'offers': offers}
 
-        offer = Offer(**validated_data)
+    # Fulfill the template with the data and send it to the internet browser
+    return render_template('offers.html', **data)
 
 
-        return redirect('/')
-    else:
-        print(form.errors.items())
+# @app.route('/add', methods=['GET', 'POST'])
+# def add_offer():
+#     form = OfferForm()
+#
+#     if form.validate_on_submit():
+#         validated_data = form.data.copy()
+#         validated_data.pop('csrf_token')
+#
+#         new_offer = Offer(**validated_data)
+#
+#         # add new offer to the database
+#         db.session.add(new_offer)
+#         db.session.commit()
+#
+#         return redirect('/')
+#
+#     return render_template('offer_form.html', form=form)
 
-    return render_template('client_form.html', form=form)
 
-# @app.route('wyszukaj_oferty/')
-# def home():
-#     clients = Offer.query.all()
-#     print(clients)
-#     return 'hello'
-
-        # db.session.add(offer)
-        # db.session.commit()
+# # @app.route('/offers_search')
+# # def home():
+# #     clients = Offer.query.all()
+# #     print(clients)
+# #     return 'hello'
+#
+#         # db.session.add(offer)
+#         # db.session.commit()
