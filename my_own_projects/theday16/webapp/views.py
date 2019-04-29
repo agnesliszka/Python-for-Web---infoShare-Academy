bab80ad94541f20eac513f7efc3f2cce1feb23e7
Agnieszka <agnesliszk@gmail.com>
2019-04-29 21:52:01 +0200

    views offers search - code added

--- my_own_projects/theday16/webapp/views.py
+++ my_own_projects/theday16/webapp/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, request
 from .forms import OfferForm, SearchForModelForm
 from .models import Offer
 from . import app
@@ -7,9 +7,8 @@ from. import db
 
 @app.route('/')
 def home():
-    offers = Offer.query.all()
-    print(offers)
-    return render_template("home.html")
+    html = render_template("home.html")
+    return html
 
 @app.route('/offers')
 def get_offers():
@@ -28,24 +27,15 @@ def brand_search():
     form = OfferForm()
     return render_template("brand_search.html", form = form)
 
-    # redirect?
-
 @app.route('/show_searched_brand', methods=['GET', 'POST'])
 def show_searched_brand():
-    return "It works"
-#     searched_offer_model = Offer.query.filter(brand=form.data['brand'])
-#     /results?show_searched_brand=Audi
+    brand = request.args.get('brand')
+    searched_offer_model = Offer.query.filter(brand==brand)
+    return render_template('show_searched_brand.html', form=searched_offer_model)
 
-#       if form.validate_on_submit():
-#         validated_data = form.data.copy()
-#         validated_data.pop('csrf_token')
-#         return redirect('/offers')
-#
-#         searched_offer_model = Offer.query.filter(brand=form.data['brand'])
-#         return render_template('offers.html', form=searched_offer_model)
-#
-# # Fulfill the template with the data and send it to the internet browser
-#     return render_template('search_for_brand.html', search_for_brand)
+
+# form = OfferForm()
+# searched_offer_model = Offer.query.filter(brand=form.data[brand])
 
 
 
